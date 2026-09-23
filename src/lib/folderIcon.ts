864f095e404536b06468e4b57efb5e97f1a2cb95
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2026-09-22 19:22:49 -0700

    folder icon qol

---
+++ src/lib/folderIcon.ts
@@ -0,0 +1,32 @@
+import { html } from "lit-html";
+
+export const folderIcon = html`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 120 120"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xml:space="preserve"
+    xmlns:serif="http://www.serif.com/"
+    style="fill-rule:evenodd;clip-rule:evenodd;stroke-miterlimit:10;"
+    class="thumb"
+  >
+    <g id="Artboard1" transform="matrix(1,0,0,1,20,20)">
+      <rect x="-20" y="-20" width="120" height="120" style="fill:none;" />
+      <clipPath id="_clip1">
+        <rect x="-20" y="-20" width="120" height="120" />
+      </clipPath>
+      <g clip-path="url(#_clip1)">
+        <g transform="matrix(1.586592,0,0,1.586592,-23.463687,-23.493017)">
+          <path
+            d="M12,70.5C7.7,70.5 4.2,67 4.2,62.7L4.2,13.7C4.2,11.4 6.1,9.5 8.4,9.5L28.6,9.5C31.5,9.5 34.2,11.1 35.5,13.7L40.3,20L68,20C72.3,20 75.8,23.5 75.8,27.8L75.8,62.8C75.8,67.1 72.3,70.6 68,70.6L12,70.5Z"
+            style="fill:var(--primary);fill-rule:nonzero;stroke:var(--accent);stroke-width:1.26px;"
+            vector-effect="non-scaling-stroke"
+          />
+        </g>
+      </g>
+    </g>
+  </svg> `;
